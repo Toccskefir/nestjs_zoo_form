0b311b606ecc05ee431a8e75b7ece196077364a3
Tóth Kevin <tothkevin0716@gmail.com>
2023-12-06 14:50:48 +0100

    Installed mysql2, created conn variable

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,14 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as mysql from 'mysql2';
 import { NewAnimalDto } from './newAnimalDto';
+
+const conn = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'zoo_test',
+}).promise();
 
 @Controller()
 export class AppController {
